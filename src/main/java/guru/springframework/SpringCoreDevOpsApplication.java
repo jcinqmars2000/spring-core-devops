a4d756cc4106b4528e5cc6f3628cccf4a0bcb28d
John Cinq-Mars <jcinqmars2000@yahoo.com>
2018-12-27 08:30:56 -0500

    Test change after fixing Jenkin's config again

--- src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -10,6 +10,7 @@ public class SpringCoreDevOpsApplication {
 		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
 
 		//this is a whitespace change test
+
 		//Change by JCM
         //Change to Test github Trigger
 	}
